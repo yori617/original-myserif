6cd5e779a2724a01012bbbea1a2e36772d7f3fac
rin.ogawa <wanoku01@yahoo.co.jp>
2020-10-27 05:16:11 +0000

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,30 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = '登録完了です。ようこそ。'
+      redirect_to @user
+    else
+      flash.now[:danger] = '登録失敗。もう一度試してみましょう。'
+      render :new
+    end
   end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
+
